88dbb458d6456087358489529a235a2e793d3c4f
PaLucena <pablolucena01@gmail.com>
2024-07-13 12:45:19 +0200

    feat: Started play page

--- srcs/requirements/frontend/scripts/login-signup.js
+++ srcs/requirements/frontend/scripts/login-signup.js
@@ -1,4 +1,34 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+	const loginForm = document.getElementById("loginForm");
+
+	loginForm.addEventListener("submit", function(event) {
+		event.preventDefault();
+
+		const formData = new FormData(loginForm);
+		const jsonData = {};
+
+		formData.forEach((value, key) => {
+			jsonData[key] = value;
+		});
+
+		fetch("http://0.0.0.0:8000/login", {
+			method: "POST",
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(jsonData)
+		})
+		.then(response => response.json)
+		.then(data => {
+			console.log("Success", data);
+		})
+		.catch((error) => {
+			console.error("Error: ", error);
+		})
+	})
+})
+
 function	login() {
 	alert("Work in progress...");
 }
