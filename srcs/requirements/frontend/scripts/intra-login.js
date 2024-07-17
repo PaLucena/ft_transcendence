d1dbe51a72c5e4756d5146c80d8d632fa2841396
ealgar-c <ealgar-c@student.42malaga.com>
2024-07-17 12:43:47 +0200

    started intra api

--- srcs/requirements/frontend/scripts/intra-login.js
+++ srcs/requirements/frontend/scripts/intra-login.js
@@ -1,3 +1,10 @@
 function	intraLogin() {
-	alert("NO");
+	fetch('https://api.intra.42.fr/v2/oauth/authorize', {
+		method: 'GET',
+		mode: 'no-cors',
+		headers: {
+			'client_id': "u-s4t2ud-781a91f2e625f3dc4397483cfabd527da78d78a6d43f5be15bfac2ea1d8fe8c6",
+			'redirect_uri': ''
+		}
+	})
 }
