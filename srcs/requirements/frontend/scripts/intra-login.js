2e179d10384c5131f88ccb1e2629a0883a4ab635
ealgar-c <ealgar-c@student.42malaga.com>
2024-07-20 11:18:50 +0200

    feat: started working on the 42pi

--- srcs/requirements/frontend/scripts/intra-login.js
+++ srcs/requirements/frontend/scripts/intra-login.js
@@ -4,7 +4,8 @@ function	intraLogin() {
 		mode: 'no-cors',
 		headers: {
 			'client_id': "u-s4t2ud-781a91f2e625f3dc4397483cfabd527da78d78a6d43f5be15bfac2ea1d8fe8c6",
-			'redirect_uri': ''
+			'redirect_uri': 'https://www.google.com',
+			'scope': 'code'
 		}
 	})
 }
