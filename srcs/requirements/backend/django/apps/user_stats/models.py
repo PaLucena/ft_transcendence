0fde722ea804d809e5042cbc68768e56812c9867
gkrusta <krustagabriela@gmail.com>
2024-07-21 19:02:17 +0200

    made user_stats_update()

--- srcs/requirements/backend/django/apps/user_stats/models.py
+++ srcs/requirements/backend/django/apps/user_stats/models.py
@@ -1,15 +1,31 @@
 from django.db import models
 from ..user.models import AppUser
+from django.utils import timezone
 
 class UserStats(models.Model):
-    user = models.OneToOneField(AppUser, on_delete=models.CASCADE, related_name='stats')
-    games_played = models.PositiveIntegerField(default=0)
-    games_played = models.PositiveIntegerField(default=0)
-    games_lost = models.PositiveIntegerField(default=0)
-    winning_streak = models.PositiveIntegerField(default=0)
-    losing_streak = models.PositiveIntegerField(default=0)
-    highest_score = models.PositiveIntegerField(default=0)
-    #average_score = models.FloatField(default=0.0)
-    #win_rate = models.FloatField(default=0.0)
-    #last_game_date = models.DateTimeField(null=True, blank=True)
-    #achievements = models.TextField(null=True, blank=True)
+	user = models.OneToOneField(AppUser, on_delete=models.CASCADE, related_name='stats')
+	games_played = models.PositiveIntegerField(default=0)
+	games_won = models.PositiveIntegerField(default=0)
+	games_lost = models.PositiveIntegerField(default=0)
+	winning_streak = models.PositiveIntegerField(default=0)
+	losing_streak = models.PositiveIntegerField(default=0)
+	highest_score = models.PositiveIntegerField(default=0)
+	average_score = models.FloatField(default=0.0)
+	win_rate = models.FloatField(default=0.0)
+	last_game_date = models.DateTimeField(null=True, blank=True)
+
+
+	def update_stats(self, won, score):
+		if won:
+			self.winning_streak += 1
+			self.games_won += 1
+			self.losing_streak = 0
+		else:
+			self.losing_streak += 1
+			self.games_lost += 1
+			self.winning_streak = 0
+		self.games_played += 1
+		self.average_score = (score + self.average_score * (self.games_played - 1)) / self.games_played
+		self.win_rate = (self.games_won / self.games_played) * 100
+		self.last_game_date = timezone.now()
+		self.save()
--- srcs/requirements/backend/django/apps/user_stats/views.py
+++ srcs/requirements/backend/django/apps/user_stats/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework import status
+from django.contrib.auth.decorators import login_required
+from srcs.requirements.backend.django.apps.user.models import AppUser
+from .models import UserStats
+from rest_framework.response import Response
 
-# Create your views here.
+# expected from frontend smth like this
+# {
+#     "player1": {
+#         "user_id": 1,
+#         "won": true,
+#         "score": 150
+#     },
+#     "player2": {
+#         "user_id": 2,
+#         "won": false,
+#         "score": 120
+#     }
+# }
+
+
+@login_required
+@api_view(["POST"])
+def user_stats_update(request):
+	try:
+		player_1_data = request.data.get(['player_1'])
+		player_2_data = request.data.get(['player_2'])
+
+		if not player_1_data or not player_2_data:
+			return Response({'error': 'Missing player data'}, status=status.HTTP_400_BAD_REQUEST)
+
+		player_1 = AppUser.objects.filter(pk=player_1.user_id)
+		player_2 = AppUser.objects.filter(pk=player_2.user_id)
+
+		stats_1 = UserStats.objects.filter(user=player_1)
+		stats_2 = UserStats.objects.filter(user=player_2)
+		stats_1.update_stats(player_1['won'], player_1['score'])
+		stats_2.update_stats(player_2['won'], player_2['score'])
+
+		return Response({'message': 'Statistics updated successfully.'}, status=status.HTTP_200_OK)
+
+	except Exception as e:
+		return Response({"error": str(e)}, status=status.HTTP_409_CONFLICT)
