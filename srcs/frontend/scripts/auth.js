e26861608327563894037ef7737500ec2448ca09
PaLucena <pablolucena01@gmail.com>
2024-07-31 11:15:32 +0200

    fix: :bug: 42 api login

--- srcs/frontend/scripts/auth.js
+++ srcs/frontend/scripts/auth.js
@@ -1,3 +1,7 @@
+function	intraLogin() {
+	window.location.href = "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-781a91f2e625f3dc4397483cfabd527da78d78a6d43f5be15bfac2ea1d8fe8c6&redirect_uri=https%3A%2F%2Flocalhost%3A8080%2Fauth&response_type=code"
+}
+
 function getApiToken() {
 	const urlParams = new URLSearchParams(window.location.search);
 	const code = urlParams.get('code');
