94319d08efb13a5b4e3fedfa6b580745c0f2c305
BredyByte <correodemimobil@gmail.com>
2024-08-10 11:20:14 +0200

    feat: Visual scripts for modal chat are implemented

--- srcs/frontend/src/components/ChatModal/ChatModal.js
+++ srcs/frontend/src/components/ChatModal/ChatModal.js
@@ -1,9 +1,39 @@
 import { Component } from "../../scripts/Component.js";
 
 export class ChatModal extends Component {
+
 	constructor() {
 		super('/components/ChatModal/chatmodal.html')
 	}
 
-	init() {}
+	init() {
+		this.initChatModal();
+	}
+
+	initChatModal() {
+		$('.modal').on('shown.bs.modal', function() {
+			$(this).find('[autofocus]').focus();
+		});
+
+		$('#chat_container').on('scroll', function() {
+			const expandedButton = $('button[aria-expanded="true"]');
+
+			if (expandedButton.length) {
+				expandedButton.click();
+			}
+		});
+
+		function triggerAnimation() {
+			$('.bounce-animation').each(function() {
+				var $element = $(this);
+				$element.removeClass('bounce');
+				setTimeout(function() {
+					$element.addClass('bounce');
+				}, 0);
+			});
+		}
+
+		setInterval(triggerAnimation, 3000);
+		triggerAnimation();
+	}
 }
