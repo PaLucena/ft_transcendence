ed609c60f5d10d43ea003b64e9518f156ef0b357
gkrusta <krustagabriela@gmail.com>
2024-08-11 15:10:35 +0200

    feat: new tournament app's views

--- srcs/backend/django/apps/tournament/models.py
+++ srcs/backend/django/apps/tournament/models.py
@@ -14,11 +14,11 @@ class Tournament(models.Model):
 	]
 
 	name = models.CharField(max_length=100, blank=False)
-	creator = models.ForeignKey(AppUser, on_delete=models.CASCADE)
+	creator = models.ForeignKey(AppUser, on_delete=models.SET_NULL)
 	created_at = models.DateTimeField(auto_now_add=True)
-	participants = models.ManyToManyField(AppUser, related_name='participants') #if deleting, needs to marked as "deleted user"
+	participants = models.ManyToManyField(AppUser, related_name='participants', deafult=list) #if deleting, needs to marked as "deleted user"
 	invitation_time_out =  models.DateTimeField(default=lambda: timezone.now() + timedelta(minutes=10))
 	type = models.CharField(max_length=10, choices=TOURNAMENT_TYPES, default=PUBLIC)
-	pending_invitations = models.ManyToManyField(AppUser, related_name='pending', deafult=list)
 	invitation_code = models.CharField(max_length=10, null=True, blank=True)
 	is_active = models.BooleanField(default=False)
+	#pending_invitations = models.ManyToManyField(AppUser, related_name='pending', deafult=list)
--- srcs/backend/django/apps/tournament/views.py
+++ srcs/backend/django/apps/tournament/views.py
@@ -1,37 +1,49 @@
-import re
 from rest_framework.decorators import api_view
 from rest_framework import status
 from django.db.models import Q
-from sympy import public
-from srcs.backend.django.apps import tournament
 from .models import AppUser
 from .models import Tournament, UserStats
 from rest_framework.response import Response
 from user.decorators import default_authentication_required
 import random
+from blockchain import create_tournament as bc_create_tournament
+
+# when private tournamnt is craeted, the creator gets the invitation code
+@api_view (["GET"])
+@default_authentication_required
+def get_code(request, tournament_id):
+	try:
+		user = request.user
+		if user == tournament.creator:
+			tournament = Tournament.objects.filter(pk=tournament_id)
+			return Response({"code": tournament.invitation_code}, status=status.HTTP_200_OK)
+	except Exception as e:
+		return Response({"error": "Code not available."}, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view (["POST"])
 @default_authentication_required
 def create_tournament(request):
 	try:
-		name = request.data.get('name')
 		creator = request.user
+		name = request.data.get('name')
+		nickname = request.data.get('nickname')
 		type = request.data.get('type')
-		if type==Tournament.PRIVATE:
-			pending_invitations = request.data.getlist('participants')
+		creator.nickname=nickname
+		creator.save()
+
+		if type == Tournament.PRIVATE:
 			invitation_code = str(random.randint(1000, 9999))
 
-		if len(pending_invitations) > 7:
-			return Response({"error": "Too many participants in the invitation list"}, status=status.HTTP_400_BAD_REQUEST)
-		
-		pending_invitations.append(creator)
 		tournament = Tournament.objects.create(
 			name=name,
 			creator=creator,
 			type=type,
-			pending_invitations=pending_invitations
+			participants=creator,
+			invitation_code=invitation_code
 		)
 
+		tournament.participants.add(creator)
 		#start countdown 10 min
 
 		return Response({"message": "Created successfully"}, status=status.HTTP_200_OK)
@@ -48,12 +60,27 @@ def close_tournament(request, tournament_id):
 		user = request.user
 		tournament = Tournament.objects.filter(pk=tournament_id)
 
-		if (user == tournament.creator):
+		if (user != tournament.creator):
 			return Response({"error": "You can't close this tournament."}, status=status.HTTP_403_FORBIDDEN)
 
-		#here
+		player_ids = list(tournament.participants.values_list('pk', flat=True))
+		
+		try:
+			bc_response= bc_create_tournament({
+				'tournament_id': tournament_id,
+				'player_ids': player_ids
+			})
+			if bc_response.status_code == 200:
+				tournament.delete()
+				return Response({"message": "Tournament closed and processed on blockchain successfully."},
+					status=status.HTTP_200_OK)
+			else:
+				return Response({"error": "Blockchain process failed."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+		
+		except Exception as e:
+			return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 	except Exception as e:
-		return Response({"error": str(e)}, status=status.HTTP_409_CONFLICT)
+		return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["POST"])
@@ -64,11 +91,22 @@ def display_tournaments(request):
 		public_tournaments = Tournament.objects.filter(
 			type=Tournament.PUBLIC, is_active=False)
 		private_tournaments = Tournament.objects.filter(
-			pending_invitations=user, type=Tournament.PRIVATE, is_active=False)
+			type=Tournament.PRIVATE, is_active=False)
 		
+		def serialize_tournament(tournament):
+			return {
+				'name': tournament.name,
+				'players': [
+					{
+						'username': 'you' if player == user else player.userame,
+	  					'avatar': player.avatar
+					} 
+					for player in tournament.participants.all()
+				]
+			}
 		response_data = {
-			'public_tournaments': public_tournaments,
-			'private_tournaments': private_tournaments
+			'public_tournaments': [serialize_tournament(tournament) for tournament in public_tournaments],
+			'private_tournaments': [serialize_tournament(tournament) for tournament in private_tournaments]
 		}
 
 		return Response(response_data, status=status.HTTP_200_OK)
@@ -83,18 +121,47 @@ def join_tournament(request, tournament_id):
 	try:
 		user = request.user
 		tournament = Tournament.objects.filter(pk=tournament_id)
+		if len(tournament.participants) < 8:
+			return Response({"Oops! Tournament is full!"}, status=status.HTTP_400_BAD_REQUEST)
+
 		if tournament.type == Tournament.PRIVATE:
-			if user not in tournament.pending_invitations.all():
-				return Response({"error": "You are not invited to this tournament."}, status=status.HTTP_403_FORBIDDEN)
 			code = request.data.get('code', '').strip()
 			if code != tournament.invitation_code:
 				return Response({"error": "Invalid invitation code."}, status=status.HTTP_403_FORBIDDEN)
+			if user  in tournament.participants.all():
+				return Response({"error": "You are already in."}, status=status.HTTP_400_BAD_REQUEST)
 			tournament.participants.add(user)
 
-		elif tournament.type == Tournament.PUBLIC and len(tournament.participants) < 8:
+		elif tournament.type == Tournament.PUBLIC:
+			if user in tournament.participants.all():
+				return Response({"error": "You are already in."}, status=status.HTTP_400_BAD_REQUEST)
 			tournament.participants.add(user)
 
 		return Response({"success": "You have joined the tournament."}, status=status.HTTP_200_OK)
 	
 	except Exception as e:
 		return Response({"Oops! Tournament is closed or doesn't exist!"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["POST"])
+@default_authentication_required
+def remove_participation(request, tournament_id):
+	try:
+		user = request.user
+		tournament = Tournament.objects.filter(pk=tournament_id)
+		if tournament.is_active == True:
+			return Response({"Oops! Tournament is closed!"}, status=status.HTTP_400_BAD_REQUEST)
+		if user in tournament.participants.all():
+			tournament.participants.remove(user)
+			if user == tournament.creator:
+				if tournament.participants.count() != 0:
+					tournament.creator = tournament.participants.first()
+
+			if tournament.participants.count() == 0:
+				tournament.delete()
+				return Response({"message": "You were the last participant, so the tournament has been deleted."}, status=status.HTTP_200_OK)
+			
+		return Response({"success": "You have been removed from the tournament."}, status=status.HTTP_200_OK)
+
+	except Exception as e:
+		return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
