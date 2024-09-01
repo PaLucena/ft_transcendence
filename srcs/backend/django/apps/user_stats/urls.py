bda6763234e4c4301bf89c6808d5debe2cfe476b
gkrusta <krustagabriela@gmail.com>
2024-09-01 14:41:16 +0200

    feat: stats done

--- srcs/backend/django/apps/user_stats/urls.py
+++ srcs/backend/django/apps/user_stats/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('user_stats_update', views.user_stats_update, name='user_stats_update'),
-	path('show_stats', views.show_stats, name='show_stats'),
+	path('player_statistics/<str:username>/', views.player_statistics, name='player_statistics'),
 ]
