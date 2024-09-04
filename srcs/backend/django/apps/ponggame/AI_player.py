dc6d29685421dde7e6bab9995a1767fa0f6f969a
pablovilchez <pablovilchez.r@gmail.com>
2024-09-04 18:47:19 +0200

    refactor ponj.js, add exceptions

--- srcs/backend/django/apps/ponggame/AI_player.py
+++ srcs/backend/django/apps/ponggame/AI_player.py
@@ -1,13 +1,10 @@
-import time
-
-
 class AiPlayer:
     def __init__(self, game):
         self.game = game
 
         # Board
         self.BOARD_START = 0 + self.game.BALL_RADIUS
-        self.BOARD_END = self.game.TABLE_WIDTH - self.game.BALL_RADIUS
+        self.BOARD_END = self.game.TABLE_HEIGHT - self.game.BALL_RADIUS
 
         # Last seen data
         self.ball_x = 0
@@ -43,56 +40,67 @@ class AiPlayer:
 
 
     def data_update(self):
-        self.ball_x = self.game.ball_x
-        self.ball_y = self.game.ball_y
-        self.ball_vel_x = self.game.ball_vel_x
-        self.ball_vel_y = self.game.ball_vel_y * self.game.ball_dir_y
-        self.ball_dir_x = self.game.ball_dir_x
-        self.ball_dir_y = self.game.ball_dir_y
-        if self.ia_side == 1:
-            self.ia_y = self.game.pad_1_y
-            self.opponent_y = self.game.pad_2_y
-        else:
-            self.ia_y = self.game.pad_2_y
-            self.opponent_y = self.game.pad_1_y
-        # print(f"AI read time: {time.time() - self.game.start_time:.3f}") # DEBUG
+        try:
+            self.ball_x = self.game.ball_x
+            self.ball_y = self.game.ball_y
+            self.ball_vel_x = self.game.ball_vel_x
+            self.ball_vel_y = self.game.ball_vel_y * self.game.ball_dir_y
+            self.ball_dir_x = self.game.ball_dir_x
+            self.ball_dir_y = self.game.ball_dir_y
+            if self.ia_side == 1:
+                self.ia_y = self.game.pad_1_y
+                self.opponent_y = self.game.pad_2_y
+            else:
+                self.ia_y = self.game.pad_2_y
+                self.opponent_y = self.game.pad_1_y
+            # print(f"AI read time: {time.time() - self.game.start_time:.3f}") # DEBUG
+        except Exception as e:
+            print(f"Error updating data (AI): {e}")
 
 
     def move(self):
-        distance = self.ia_y - self.new_position
-        if distance < 0 and distance < -self.game.PADDLE_SPEED:
-            self.game.move_paddle(self.ia_side, 1)
-            self.ia_y += self.game.PADDLE_SPEED
-        elif distance > 0 and distance > self.game.PADDLE_SPEED:
-            self.game.move_paddle(self.ia_side, -1)
-            self.ia_y -= self.game.PADDLE_SPEED
+        try:
+            distance = self.ia_y - self.new_position
+            if distance < 0 and distance < -self.game.PADDLE_SPEED:
+                self.game.move_paddle(self.ia_side, 1)
+                self.ia_y += self.game.PADDLE_SPEED
+            elif distance > 0 and distance > self.game.PADDLE_SPEED:
+                self.game.move_paddle(self.ia_side, -1)
+                self.ia_y -= self.game.PADDLE_SPEED
+        except Exception as e:
+            print(f"Error moving paddle (AI): {e}")
 
 
     def think(self):
-        if (((self.ia_side == 1 and self.ball_dir_x < 0) or
-                (self.ia_side == 2 and self.ball_dir_x > 0)) and
-                self.game.game_state == "playing"):
-            self.calc_collision_point()
-            self.calc_target_point()
-            self.calc_new_position()
-            # self.print_data() # DEBUG
-        else:
-            self.new_position = self.game.TABLE_HEIGHT / 2
+        try:
+            if (((self.ia_side == 1 and self.ball_dir_x < 0) or
+                    (self.ia_side == 2 and self.ball_dir_x > 0)) and
+                    self.game.game_state == "playing"):
+                self.calc_collision_point()
+                self.calc_target_point()
+                self.calc_new_position()
+            else:
+                self.new_position = self.game.TABLE_HEIGHT / 2
+        except Exception as e:
+            print(f"Error thinking (AI): {e}")
 
 
     def calc_collision_point(self):
-        if self.ia_side == 2:
-            self.distance_x = (self.game.TABLE_WIDTH - self.game.GOAL_TAB_MARGIN
-                               - self.ball_x - self.game.BALL_RADIUS)
-        else:
-            self.distance_x = self.ball_x - self.game.GOAL_TAB_MARGIN - self.game.BALL_RADIUS
-        self.col_x_time = self.distance_x / abs(self.ball_vel_x)
-        self.collision_y = self.ball_y + self.ball_vel_y * self.col_x_time
-        while self.collision_y < self.BOARD_START or self.collision_y > self.BOARD_END:
-            if self.collision_y < self.BOARD_START:
-                self.collision_y = (self.collision_y - self.game.BALL_RADIUS) * -1
+        try:
+            if self.ia_side == 2:
+                self.distance_x = (self.game.TABLE_WIDTH - self.game.PADDLE_TAB_MARGIN
+                                   - self.ball_x - self.game.BALL_RADIUS)
             else:
-                self.collision_y = 2 * self.BOARD_END - self.collision_y
+                self.distance_x = self.ball_x - self.game.PADDLE_TAB_MARGIN - self.game.BALL_RADIUS
+            self.col_x_time = self.distance_x / self.ball_vel_x
+            self.collision_y = self.ball_y + self.ball_vel_y * self.col_x_time
+            while self.collision_y < self.BOARD_START or self.collision_y > self.BOARD_END:
+                if self.collision_y < self.BOARD_START:
+                    self.collision_y = (self.collision_y - self.game.BALL_RADIUS) * -1
+                else:
+                    self.collision_y = 2 * self.BOARD_END - self.collision_y
+        except Exception as e:
+            print(f"Error calculating collision point (AI): {e}")
 
 
     def calc_target_point(self):
@@ -104,13 +112,13 @@ class AiPlayer:
 
 
     def print_data(self):
+        print("*************************")
         print("Ball: ", self.ball_x, self.ball_y)
         print("Velocity: ", self.ball_vel_x, self.ball_vel_y)
-        print("IA position: ", self.ia_y)
         print("Opponent position: ", self.opponent_y)
         print("Distance in X to AI: ", self.distance_x)
         print("Time to collision: ", self.col_x_time)
         print("Collision point in Y: ", self.collision_y)
         print("Target point: ", self.target_point)
         print("Position to reach: ", self.new_position)
-        print("\n")
+        print("*************************")
