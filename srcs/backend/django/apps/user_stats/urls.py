dcecac3c33eacba0a3bca76f0a4ac9ead562695a
gkrusta <krustagabriela@gmail.com>
2024-09-09 11:23:00 +0200

    player_comparison

--- srcs/backend/django/apps/user_stats/urls.py
+++ srcs/backend/django/apps/user_stats/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('player_statistics/<str:username>/', views.player_statistics, name='player_statistics'),
+	path('player_comparison/<str:username>/', views.player_comparison, name='player_comparison'),
 ]
