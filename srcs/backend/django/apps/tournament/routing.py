f1797ca0640f376b65950e733f2ae3e1499496c7
BredyByte <correodemimobil@gmail.com>
2024-09-19 21:45:21 +0200

    feat: Prevent duplicate UserWebSocket initialization

--- srcs/backend/django/apps/tournament/routing.py
+++ srcs/backend/django/apps/tournament/routing.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from user.consumers import OnlineStatusConsumer
 from tournament.consumers import TournamentConsumer
 
 websocket_urlpatterns = [
-    path("ws/tournament-users/", OnlineStatusConsumer.as_asgi()),
     path('ws/tournament/<str:tournament_name>/', TournamentConsumer.as_asgi()),
 ]
