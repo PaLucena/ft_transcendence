4fbc3ad4579e9f765e27781fba1716716b67a450
BredyByte <correodemimobil@gmail.com>
2024-09-21 09:44:41 +0200

    fix: Invite Models are rewritten

--- srcs/backend/django/apps/rtchat/admin.py
+++ srcs/backend/django/apps/rtchat/admin.py
@@ -1,11 +1,33 @@
 from django.contrib import admin
-from rtchat.models import ChatGroup, GroupMessage, Block, Invite
+from rtchat.models import ChatGroup, GroupMessage, Block, InviteRoom, InviteUser
 
-class InviteAdmin(admin.ModelAdmin):
-    list_display = ("sender", "receiver", "is_accepted", "is_cancelled")
+
+class InviteUserInline(admin.TabularInline):
+    model = InviteUser
+    extra = 0
+
+
+class InviteRoomAdmin(admin.ModelAdmin):
+    list_display = (
+        "group_name",
+        "created_at",
+        "expires_at",
+        "is_accepted",
+        "is_cancelled",
+    )
+    search_fields = ("group_name",)
+    inlines = [InviteUserInline]
+    list_filter = ("is_accepted", "is_cancelled")
+
+
+class InviteUserAdmin(admin.ModelAdmin):
+    list_display = ("invite_room", "user", "status")
+    list_filter = ("status",)
+    search_fields = ("invite_room__group_name", "user__username")
+
 
 admin.site.register(ChatGroup)
 admin.site.register(GroupMessage)
 admin.site.register(Block)
-admin.site.register(Invite, InviteAdmin)
-
+admin.site.register(InviteRoom, InviteRoomAdmin)
+admin.site.register(InviteUser, InviteUserAdmin)
