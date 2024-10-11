ea9bb039e6d7f1638dfa9bdf95eacfb10c1f5c3d
BredyByte <correodemimobil@gmail.com>
2024-10-11 13:08:20 +0200

    fix: Admin page styles

--- srcs/backend/django/apps/core/settings.py
+++ srcs/backend/django/apps/core/settings.py
@@ -202,7 +202,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "api/django/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
